621543b32c680083fe713170cee3dcd643afaa73
jinjin.you <676630964@qq.com>
2019-02-19 17:46:46 +0800

    05.Dio基础_引入和简单的Get请求

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,24 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    getHttp();
     return Scaffold(
         body: Center(
       child: Text('商城首页'),
     ));
+  }
+
+  void getHttp() async {
+    try {
+      Response response;
+      response = await Dio().get(
+          "https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian?name=长腿小姐姐");
+      return print(response);
+    } catch (e) {
+      return print(e);
+    }
   }
 }
