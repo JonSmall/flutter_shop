e290d4cd6ded52d7b5b50ccef90161af533b5bfc
youjinjin <676630964@qq.com>
2019-02-23 13:44:33 +0800

    08.Dio基础_伪造请求头获取数据

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import '../config/httpHeaders.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,70 +8,47 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  TextEditingController typeController = TextEditingController();
-  String showText = '欢迎您来到美好人间高级会所';
-
+  String showText = '还没有请求数据';
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Scaffold(
-          // 技术胖是采用SingleChildScrollView解决键盘遮挡内容的问题的
-          // 经过测试，效果没有resizeToAvoidBottomPadding好
-          resizeToAvoidBottomPadding: false,
-          appBar: AppBar(
-            title: Text('美好人间'),
+        appBar: AppBar(
+          title: Text('请求远程数据'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              RaisedButton(
+                onPressed: _jike,
+                child: Text('请求数据'),
+              ),
+              Text(showText)
+            ],
           ),
-          body: Container(
-            child: Column(
-              children: <Widget>[
-                TextField(
-                  controller: typeController,
-                  decoration: InputDecoration(
-                      contentPadding: EdgeInsets.all(10.0),
-                      labelText: '美女类型',
-                      helperText: '请输入你喜欢的类型'),
-                  autofocus: false,
-                ),
-                RaisedButton(
-                  onPressed: _choiceAction,
-                  child: Text('选择完毕'),
-                ),
-                Text(
-                  showText,
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 1,
-                )
-              ],
-            ),
-          )),
+        ),
+      ),
     );
   }
 
-  void _choiceAction() {
-    print('开始选择你喜欢的类型................');
-    if (typeController.text.toString() == '') {
-      showDialog(
-          context: context,
-          builder: (context) => AlertDialog(title: Text('美女类型不能为空')));
-    } else {
-      getHttp(typeController.text.toString()).then((val) {
-        setState(() {
-          showText = val['data']['name'].toString();
-        });
+  void _jike() {
+    print('开始向极客时间请求数据.....................');
+    getHttp().then((val) {
+      setState(() {
+        showText = val['data'].toString();
       });
-    }
+    });
   }
 
-  Future getHttp(String typeText) async {
+  Future getHttp() async {
     try {
       Response response;
-      var data = {'name': typeText};
-      response = await Dio().post(
-          "https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/post_dabaojian",
-          queryParameters: data);
+      Dio dio = Dio();
+      dio.options.headers = httpHeaders;
+      response =
+          await dio.get('https://time.geekbang.org/serv/v1/column/topList');
+      print(response);
       return response.data;
-    } catch (e) {
-      return print(e);
-    }
+    } catch (e) {}
   }
 }
