91de304d470f0a1cfb029e03d8de79c512c22264
jinjin.you <676630964@qq.com>
2019-02-26 11:34:41 +0800

    10.首页_FlutterSwiper轮播效果制作

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import '../service/service_method.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   @override
@@ -24,8 +26,44 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text('百姓生活+'),
       ),
-      body: SingleChildScrollView(
-        child: Text(homePageContent),
+      body: FutureBuilder(
+        future: getHomePageContent(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            var data = json.decode(snapshot.data.toString());
+            List<Map> swiper = (data['data']['slides'] as List).cast();
+            return Column(
+              children: <Widget>[SwiperDiy(swiperDateList: swiper)],
+            );
+          } else {
+            return Center(
+              child: Text('加载中.....'),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+// 首页轮播组件
+class SwiperDiy extends StatelessWidget {
+  final List swiperDateList;
+  SwiperDiy({Key key, this.swiperDateList}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 333,
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network(
+            '${swiperDateList[index]['image']}',
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: 3,
+        pagination: SwiperPagination(),
+        autoplay: true,
       ),
     );
   }
