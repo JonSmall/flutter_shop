a1de053da22b66793aaa31aa2291a01a7999129a
youjinjin <676630964@qq.com>
2019-02-27 23:53:31 +0800

    12.首页_GridView类别导航制作

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -23,8 +23,6 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
-
     print('设备像素密度：${ScreenUtil.pixelRatio}');
     print('设备的高：${ScreenUtil.screenHeight}');
     print('设备的宽：${ScreenUtil.screenWidth}');
@@ -39,8 +37,12 @@ class _HomePageState extends State<HomePage> {
           if (snapshot.hasData) {
             var data = json.decode(snapshot.data.toString());
             List<Map> swiper = (data['data']['slides'] as List).cast();
+            List<Map> navigatorList = (data['data']['category'] as List).cast();
             return Column(
-              children: <Widget>[SwiperDiy(swiperDataList: swiper)],
+              children: <Widget>[
+                SwiperDiy(swiperDataList: swiper),
+                TopNavigator(navigatorList: navigatorList)
+              ],
             );
           } else {
             return Center(
@@ -72,6 +74,46 @@ class SwiperDiy extends StatelessWidget {
         itemCount: 3,
         pagination: SwiperPagination(),
         autoplay: true,
+      ),
+    );
+  }
+}
+
+class TopNavigator extends StatelessWidget {
+  final List navigatorList;
+  TopNavigator({Key key, this.navigatorList}) : super(key: key);
+
+  Widget _gridItemUI(BuildContext context, item) {
+    return InkWell(
+      onTap: () {
+        print('点击了导航');
+      },
+      child: Column(
+        children: <Widget>[
+          Image.network(
+            item['image'],
+            width: ScreenUtil().setWidth(95),
+          ),
+          Text(item['mallCategoryName'])
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (this.navigatorList.length > 10) {
+      this.navigatorList.removeRange(10, this.navigatorList.length);
+    }
+    return Container(
+      height: ScreenUtil().setHeight(320),
+      padding: EdgeInsets.all(3.0),
+      child: GridView.count(
+        crossAxisCount: 5,
+        padding: EdgeInsets.all(5.0),
+        children: navigatorList.map((item) {
+          return _gridItemUI(context, item);
+        }).toList(),
       ),
     );
   }
