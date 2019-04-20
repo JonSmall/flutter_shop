62971cd8abcd831baf4dfa0f4b43fec5e537fc71
youjinjin <676630964@qq.com>
2019-04-20 23:13:47 +0800

    62.购物车_首页Provide化 让跳转随心所欲

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import './provide/child_category.dart';
 import './provide/category_goods_list.dart';
 import './provide/details_info.dart';
 import './provide/cart.dart';
+import './provide/current_index.dart';
 import 'animation/open_animation.dart';
 import 'package:fluro/fluro.dart';
 import './routers/routes.dart';
@@ -17,6 +18,7 @@ void main() {
   var categoryGoodsListProvide = CategoryGoodsListProvide();
   var detailsInfoProvide = DetailsInfoProvide();
   var cartProvide = CartProvide();
+  var currentIndexProvide = CurrentIndexProvide();
   var providers = Providers();
 
   providers
@@ -25,7 +27,8 @@ void main() {
     ..provide(
         Provider<CategoryGoodsListProvide>.value(categoryGoodsListProvide))
     ..provide(Provider<DetailsInfoProvide>.value(detailsInfoProvide))
-    ..provide(Provider<CartProvide>.value(cartProvide));
+    ..provide(Provider<CartProvide>.value(cartProvide))
+    ..provide(Provider<CurrentIndexProvide>.value(currentIndexProvide));
 
   runApp(ProviderNode(child: MyApp(), providers: providers));
 }
